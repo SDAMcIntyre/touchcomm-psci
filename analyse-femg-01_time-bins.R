96f636a64c8a8155859f51caa12c7cef96a55269
S McIntyre <sdamcintyre+git@gmail.com>
2021-04-15 22:11:16 +0200

    made time plot prettier

--- analyse-femg-01_time-bins.R
+++ analyse-femg-01_time-bins.R
@@ -21,7 +21,7 @@ clean_bins <- function(df, prefixes, pfkeep = 0) {
   return(df)
 }
 
-myboot <- function(x) { mean_cl_boot(x, B=10000) }
+myboot <- function(x) { mean_cl_boot(x, B=1000) } 
 
 time_plot <- function(df, muscle) {
   df %>% 
@@ -31,10 +31,13 @@ time_plot <- function(df, muscle) {
     stat_summary(fun.data = 'myboot', geom = 'ribbon', alpha = 0.3) +
     stat_summary(fun = 'mean', geom = 'line') +
     stat_summary(fun = 'mean', geom = 'point') +
-    scale_x_continuous(breaks = seq(min(df$time),max(df$time),1)) +
+    scale_x_continuous(
+      breaks = seq(min(df$time),max(df$time),1), expand = c(0.01,0.01)
+      ) +
     coord_cartesian(ylim = c(-0.6,1.1)) +
     theme_bw() +
-    labs(x = 'Time (seconds)', y = 'Muscle Activity (z)')
+    labs(x = 'Time (seconds)', y = 'Muscle Activity (z)', 
+         colour = NULL, fill = NULL, shape = NULL, linetype = NULL)
 }
 
 #### main ####
@@ -77,21 +80,46 @@ femg.to.plot <- femg.binned %>%
   left_join(cutoffs, by = 'cued') %>% 
   filter(time <= cut.time)
 
-t.zyg.plot <- femg.to.plot %>% time_plot(t.zyg.zfixed) + labs(title = 'Sender')
-r.zyg.plot <- femg.to.plot %>% time_plot(r.zyg.zfixed) + labs(title = 'Receiver')
-t.cor.plot <- femg.to.plot %>% time_plot(t.cor.zfixed) + labs(title = 'Sender')
-r.cor.plot <- femg.to.plot %>% time_plot(r.cor.zfixed) + labs(title = 'Receiver')
+#### plot ####
 
-quartz(width = 14, height = 7.6); plot(1:10) 
-t.zyg.plot / r.zyg.plot +
-  plot_layout(guides = 'collect') +
-  plot_annotation(tag_levels = 'A', title = 'Zygomaticus')
-ggsave(paste0('figures/femg_zyg_binned-',round(bin.sec*1000),'ms-windows.png'))
-ggsave(paste0('figures/femg_zyg_binned-',round(bin.sec*1000),'ms-windows.svg'))
+t.zyg.plot <- femg.to.plot %>% time_plot(t.zyg.zfixed) + 
+  labs(x = NULL, title = 'A. Sender Zygomaticus')
+r.zyg.plot <- femg.to.plot %>% time_plot(r.zyg.zfixed) + 
+  labs(x = NULL, title = 'B. Receiver Zygomaticus')
+t.cor.plot <- femg.to.plot %>% time_plot(t.cor.zfixed) + 
+  labs(x = NULL, title = 'C. Sender Corrugator')
+r.cor.plot <- femg.to.plot %>% time_plot(r.cor.zfixed) +
+  labs(title = 'D. Receiver Corrugator')
 
-quartz(width = 14, height = 7.6); plot(1:10) 
-t.cor.plot / r.cor.plot +
-  plot_layout(guides = 'collect') +
-  plot_annotation(tag_levels = 'A', title = 'Corrugator')
-ggsave(paste0('figures/femg_cor_binned-',round(bin.sec*1000),'ms-windows.png'))
-ggsave(paste0('figures/femg_cor_binned-',round(bin.sec*1000),'ms-windows.svg'))
+quartz(width = 7.8, height = 10.2); plot(1:10) 
+
+design <- "
+A
+B
+B
+B
+B
+C
+C
+C
+C
+D
+D
+D
+D
+E
+E
+E
+E
+"
+guide_area() + 
+  t.zyg.plot + 
+  r.zyg.plot + 
+  t.cor.plot + 
+  r.cor.plot +
+  plot_layout(design = design, guides = 'collect') & 
+  theme(legend.position = 'top') & 
+  guides(colour = guide_legend(nrow = 1))
+ggsave(paste0('figures/femg_binned-',round(bin.sec*1000),'ms-windows.png'))
+ggsave(paste0('figures/femg_binned-',round(bin.sec*1000),'ms-windows.svg'))
+
